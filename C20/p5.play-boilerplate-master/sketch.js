fea28c919a145a7e9e2a6b5f0b1d90ee5f06b060
JamesJudah <79319814+JamesJudah@users.noreply.github.com>
2021-04-19 20:31:00 +0530

    Add files via upload

--- C20/p5.play-boilerplate-master/sketch.js
+++ C20/p5.play-boilerplate-master/sketch.js
@@ -1,42 +1,73 @@
 var movingRectangle , fixedRectangle;
+var gameObject1 , gameObject2 , gameObject3 , gameObject4;
 
 function setup() {
-  createCanvas(1200,400);
-  fixedRectangle = createSprite(600, 400, 50, 80);
+  createCanvas(1200,800);
+  fixedRectangle = createSprite(600, 200, 50, 80);
   fixedRectangle.shapeColor = "green";
+  fixedRectangle.velocityY = 5;
 
-
-  movingRectangle = createSprite(400, 200, 80, 30);
+  movingRectangle = createSprite(600, 700, 80, 30);
   movingRectangle.shapeColor = "green";
+  movingRectangle.velocityY = -5;
   
-  
+ /* gameObject1 = createSprite(100,100,50,50);
+  gameObject1.shapeColor = "green";
+
+
+  gameObject2 = createSprite(200,100,50,50);
+  gameObject2.shapeColor = "green";
+
+  gameObject3 = createSprite(300,100,50,50);
+  gameObject3.shapeColor = "green";
+
+  gameObject4 = createSprite(400,100,50,50);
+  gameObject4.shapeColor = "green";*/
+
 }
 
 function draw() {
   background(255,255,255);  
   
-  movingRectangle.x = World.mouseX;
+bounceOff(movingRectangle , fixedRectangle);
+
+
+
+
+
+  /*movingRectangle.x = World.mouseX;
   movingRectangle.y = World.mouseY;
 
   console.log(movingRectangle.x - fixedRectangle.x);
 
-  if(movingRectangle.x - fixedRectangle.x < fixedRectangle.width/2 + movingRectangle.width/2  && 
-   fixedRectangle.x - movingRectangle.x < fixedRectangle.width/2 + movingRectangle.width/2 && 
-   fixedRectangle.y - movingRectangle.y < fixedRectangle.height/2 + movingRectangle.height/2 &&
-   movingRectangle.y -  fixedRectangle.y < fixedRectangle.height/2 + movingRectangle.height/2)  {
+  if(isTouching(movingRectangle , gameObject1))  {
+
+  
 
     movingRectangle.shapeColor = "red";
-    fixedRectangle.shapeColor = "red";
-  }
- else{
-    movingRectangle.shapeColor = "green";
-    fixedRectangle.shapeColor = "green";
+    gameObject1.shapeColor = "red";
+
+ } else {
+
+     movingRectangle.shapeColor = "green";
+     gameObject1.shapeColor     = "green";
 
 
- }
-     
+ }*/
+  
+
+ 
+
+
+
 
   drawSprites();
 
   
 }
+
+
+
+
+
+
